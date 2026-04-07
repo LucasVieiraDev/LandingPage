84e591df9eccd75081a6c820ee58c075c7a902b6
Lucas Vieira <118188967+LucasVieiraDev@users.noreply.github.com>
2026-04-06 21:52:25 -0300

    update nix site

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,16 @@ const navLinks = document.getElementById('nav-links');
 const icon = mobileBtn.querySelector('i');
 const header = document.getElementById('header')
 const linkNavbar = document.querySelector('.link-navbar')
+const navBar = document.querySelector('.navbar ul')
+const navBarLi = document.querySelector('.navbar li')
+
+var lastScrollTop = 0;
+var scrollTop = 0;
+
+linkNavbar.addEventListener('click', () => {
+    mobileBtn.click()
+    header.style.top ='-130px'
+})
 
 mobileBtn.addEventListener('click', () => {
     navLinks.classList.toggle('show');
@@ -11,12 +21,27 @@ mobileBtn.addEventListener('click', () => {
 });
 
 window.addEventListener('scroll', () =>{
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop){
+        header.style.top ='-130px'
+    } else {
+        header.style.top = '0'
+
+    }
+    lastScrollTop = scrollTop;
+
+
+
     if (window.scrollY > 0) {
         header.style.boxShadow = '0 4px 10px rgba(0, 0, 0, 0.1)';
+      
+
     } else {
         header.style.boxShadow = 'none'
+
+
     }
 })
-linkNavbar.addEventListener('click', () => {
-    mobileBtn.click()
-})
+
+
+
